545dd768ca61c12a667de2883b42580f7ce8e5a7
Antonio Neto <antonioneto.mec@gmail.com>
2023-01-23 17:28:15 -0300

    Initial commit

---
+++ produtoscrudspring/src/main/java/com/example/produtoscrudspring/ProdutoscrudspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.produtoscrudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoscrudspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoscrudspringApplication.class, args);
+	}
+
+}
